447393ce59ccbe8b674abccb2ab74bf2a8c3cc42
dac <dac@students.acts.in>
2024-12-18 19:23:48 +0530

    addingfeature

--- Main.java
+++ Main.java
@@ -13,33 +13,5 @@ public class Main{
         System.out.print("Enter third number: ");
         int num3 = sc.nextInt();
         int result = 0;
-
-        if(num1>num2){
-            if(num2>num3){
-                result = num1+num2;
-            }else{
-                result = num1+num3;
-            }
-        }
-        else if(num2>num1){
-            if(num1>num3){
-                result= num2+num1;
-            }else{
-                result = num2+num3;
-            }
-        }else{
-            if(num1>num2){
-                result = num3+num1;
-            }else{
-                result = num3+num2;
-            }
-        }
-
-        float average = (num1+num2+num3)/3;
-    
-
-        System.out.println("result:"+result);
-        System.out.println("average:"+average);
-        sc.close();
     }
 }
